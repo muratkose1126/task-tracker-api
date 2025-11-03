71cc55600f08291940dd07f826183c495b8da527
Murat KÖSE <muratkose1126@gmail.com>
2025-11-03 22:19:53 +0300

    Add Project and Task migrations and models with relations

---
+++ app/Models/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
