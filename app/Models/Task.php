448323807f6449cdec87bcd4942e08d89e76c6a8
Murat KÖSE <muratkose1126@gmail.com>
2025-11-07 23:03:58 +0300

    fix(Task): add missing enum casts in Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\TaskStatus;
+use App\Enums\TaskPriority;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
