dc0fb5d45b9a8a70b8815405e93cc935f3defa36
Murat KÖSE <muratkose1126@gmail.com>
2025-11-10 01:44:46 +0300

    feat(task): integrate Spatie Media Library into Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,10 +7,13 @@ use App\Enums\TaskPriority;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Task extends Model
+class Task extends Model implements HasMedia
 {
     use HasFactory, SoftDeletes;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'project_id',
@@ -41,5 +44,10 @@ class Task extends Model
     public function comments()
     {
         return $this->hasMany(TaskComment::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('attachments');
     }
 }
